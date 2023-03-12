0042542ed049402b73bfb6d26bed0ad3414941fc
AnSupp <55513899+AnSupp@users.noreply.github.com>
2023-03-12 23:31:39 +0300

    final

--- FarmingGaming/Assets/Scripting/Code/UI/InteractiveButtonLeha.cs
+++ FarmingGaming/Assets/Scripting/Code/UI/InteractiveButtonLeha.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InteractiveButtonLeha : MonoBehaviour
 {
@@ -14,6 +15,10 @@ public class InteractiveButtonLeha : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         investigateButton.SetActive(true);
+        if (col.tag == "EnemyOnMap")
+        {
+            SceneManager.LoadScene(3);
+        }
     }
     private void OnTriggerExit2D(Collider2D col)
     {
